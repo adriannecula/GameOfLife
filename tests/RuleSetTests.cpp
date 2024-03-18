553b81151796bf81bd8e417319dd293ca6e6f62b
adrian necula <adrian.necula@live.com>
2024-03-18 18:00:00 +0200

    Updated test structure

--- tests/RuleSetTests.cpp
+++ tests/RuleSetTests.cpp
@@ -2,11 +2,12 @@
 #include <utility>
 #include <iostream>
 #include <controller/RuleSet.hpp>
+#include <TestGrids.hpp>
 
 #include <gtest/gtest.h>
 TEST(Example, MyTest)
 {
-    std::pair<std::vector<uint8_t>,uint16_t> grid{{1,1,1,1},4};
+    std::pair<std::vector<uint8_t>,uint16_t> grid = helpers::getStandardGrid();
     RuleSet game{grid};
     game.calculate();
     std::cout<<"it's alive\n";
---
+++ tests/helpers/TestGrids.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <utility>
+#include <cstdint>
+namespace helpers
+{
+    std::pair<std::vector<uint8_t>,uint16_t>  getStandardGrid();
+   
+    
+}
+
